3def388f271d5ffafd937b205dfc8954b96fa002
Erick Eduardo <edu.2010@live.com.mx>
2021-02-16 04:24:05 -0600

    Se limpio el codigo de lso comentarios innecesarios en el modulo Asincronismo

--- 31_promesas.js
+++ 31_promesas.js
@@ -3,11 +3,11 @@ const PEOPLE_URL = 'people/:id'
 const opts = { crossDomain: true }
 
 function obtenerPersonaje(id) {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {//A partir de aqui se hace el llamado asincrono
     const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
     $
       .get(url, opts, function (data) {
-        resolve(data)
+        resolve(data)//El resolve no se ejecuta hasta que el get sea exitoso
       })
       .fail(() => reject(id))
   })
--- 33_multiples_promesas_en_paralelo.js
+++ 33_multiples_promesas_en_paralelo.js
@@ -18,41 +18,13 @@ function onError(id) {
 }
 
 var ids = [1, 2, 3, 4, 5, 6, 7]
+
 // var promesas = ids.map(function (id) {
 //   return obtenerPersonaje(id)
 // })
+
 var promesas = ids.map(id => obtenerPersonaje(id))
 Promise
   .all(promesas)
   .then(personajes => console.log(personajes))
   .catch(onError)
-
-// obtenerPersonaje(1)
-//   .then(personaje1 => {
-//     console.log(`El personaje 1 es ${personaje1.name}`)
-//     return obtenerPersonaje(2)
-//   })
-//   .then(personaje2 => {
-//     console.log(`El personaje 2 es ${personaje2.name}`)
-//     return obtenerPersonaje(3)
-//   })
-//   .then(personaje3 => {
-//     console.log(`El personaje 3 es ${personaje3.name}`)
-//     return obtenerPersonaje(4)
-//   })
-//   .then(personaje4 => {
-//     console.log(`El personaje 4 es ${personaje4.name}`)
-//     return obtenerPersonaje(5)
-//   })
-//   .then(personaje5 => {
-//     console.log(`El personaje 5 es ${personaje5.name}`)
-//     return obtenerPersonaje(6)
-//   })
-//   .then(personaje6 => {
-//     console.log(`El personaje 6 es ${personaje6.name}`)
-//     return obtenerPersonaje(7)
-//   })
-//   .then(personaje7 => {
-//     console.log(`El personaje 7 es ${personaje7.name}`)
-//   })
-//   .catch(onError)
